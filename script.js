e8f8b8afcfb1292fef67859dd43e58a8f60480f0
Kaleek03 <Kadec208@gmail.com>
2023-01-25 17:22:17 -0700

    made about us link

---
+++ script.js
@@ -0,0 +1,8 @@
+//function to change page on website for contact forum and about us
+
+ aboutUs = document.getElementById('about-us')
+ contactForum = document.getElementById('contact-forum')
+
+ aboutUs.addEventListener('click', aboutLink() {
+    
+ })
